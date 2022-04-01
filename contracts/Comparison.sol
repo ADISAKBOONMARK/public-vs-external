6c441c5ffd096bc00fca39d0d468d995791ee927
ADISAKBOONMARK <adisak0661013@gmail.com>
2022-04-01 13:35:47 +0700

    Add contract

---
+++ contracts/Comparison.sol
@@ -0,0 +1,113 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.0;
+
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+/// @title Comparison.sol
+/// @dev Comparison between public and external. NOTE: The lab will be a simulation, Calculation pricing of the land.
+contract Comparison is Pausable, Ownable {
+
+    /// @dev Emitted when the price changed.
+    event PriceChanged(address actionBy, uint256 oldPrice, uint256 newPrice);
+
+    /// @dev Emitted when the land was added.
+    event LandAdded(address createBy, uint256 id);
+
+    /// @dev Struct of land.
+    /// @param id Id of land.
+    /// @param width Wdth of land.
+    /// @param length Length of land.
+    /// @param area Area of land.
+    /// @param price Price of land.
+    struct Land {
+        uint256 id;
+        uint256 width;
+        uint256 length;
+        uint256 area;
+        uint256 price;
+        address owner;
+    }
+
+    /// @dev Land by owner.
+    mapping(address => Land) public land;
+
+    /// @dev Price per unit. (USD)
+    uint256 public price = 100; 
+
+    /// @dev Decimal of number.
+    uint256 public decimal = 100; 
+
+    /// @dev Use for pause state.
+    function pause() external onlyOwner {
+        _pause();
+    }
+
+    /// @dev Use for unpause state.
+    function unpause() external onlyOwner {
+        _unpause();
+    }
+
+    /// @dev Use for setting the price of land per unit.
+    function setPrice(uint256 _price) external onlyOwner {
+        require(price != _price, "Comparison: Price is already set");
+        emit PriceChanged(msg.sender, price, _price);
+        price = _price;
+    }
+
+    /// @dev Use for calculating the land area. (public)
+    /// @param _x Rectangle width. (meter)
+    /// @param _y Rectangle length. (meter)
+    function calculateWithPublic(uint256 _x, uint256 _y) public pure returns(uint256) {
+        return _x * _y;
+    }
+
+    /// @dev Use for calculating the land area. (external)
+    /// @param _x Rectangle width. (meter)
+    /// @param _y Rectangle length. (meter)
+    function calculateWithExternal(uint256 _x, uint256 _y) external pure returns(uint256) {
+        return _x * _y;
+    }
+
+    /// @dev Use for calculating the price of land.
+    /// @param _area Area of land.
+    function calculatePrice(uint256 _area) internal view returns(uint256) {
+        return _area * price;
+    }
+
+    /// @dev Use for comparison rectangle area. (public)
+    /// @param _x Rectangle width. (meter)
+    /// @param _y Rectangle length. (meter)
+    /// @param _salt Salt for random id.
+    function addLandWithPublic(uint256 _x, uint256 _y, uint256 _salt) external onlyOwner {
+        uint256 area = calculateWithPublic(_x, _y);
+        uint256 id = uint256(keccak256(abi.encodePacked(block.number, msg.sender, _salt)));
+        land[msg.sender] = Land({
+            id: id,
+            width: _x,
+            length: _y,
+            area: area,
+            price: calculatePrice(area),
+            owner: msg.sender
+        });
+        emit LandAdded(msg.sender, id);
+    }
+
+    /// @dev Use for comparison rectangle area. (external)
+    /// @param _x Rectangle width. (meter)
+    /// @param _y Rectangle length. (meter)
+    /// @param _salt Salt for random id.
+    function addLandWithExternal(uint256 _x, uint256 _y, uint256 _salt) external onlyOwner {
+        uint256 area = this.calculateWithExternal(_x, _y);
+        uint256 id = uint256(keccak256(abi.encodePacked(block.number, msg.sender, _salt)));
+        land[msg.sender] = Land({
+            id: id,
+            width: _x,
+            length: _y,
+            area: area,
+            price: calculatePrice(area),
+            owner: msg.sender
+        });
+        emit LandAdded(msg.sender, id);
+    }
+}
